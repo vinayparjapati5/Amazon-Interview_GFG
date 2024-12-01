546ac9c5c59ab3a68bd3658f81043d6c2ce21720
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-12-01 13:02:40 +0530

    Create Intersection of 2LL.cpp

---
+++ Linked List/Intersection of 2LL.cpp
@@ -0,0 +1,85 @@
+    /*ListNode *collisionpt(ListNode*t1, ListNode*t2, int d){
+        while(d){
+            d--;
+            t2 = t2->next;
+        }
+        while(t1!=t2){
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        return t1;
+    } */
+
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+         
+        //using hashing
+
+        /*unordered_map<ListNode* , int>mpp;
+        
+        ListNode*temp = headA;
+
+        while(temp){
+            mpp[temp]=1;
+            temp = temp->next;
+        }
+
+        temp = headB;
+
+        while(temp){
+            if(mpp.find(temp)!=mpp.end()){
+                return temp;
+            }
+            temp = temp->next;
+        }
+        return NULL;
+
+        // BETTER APPROACH
+/*
+        ListNode*t1 = headA, *t2 = headB;
+        int n1 = 0, n2 = 0;
+
+        while(t1){
+            n1++;
+            t1 = t1->next;
+        }
+
+        while(t2){
+            n2++;
+            t2 = t2->next;
+        }
+
+        if(n1<n2){
+            return collisionpt(headA, headB, n2-n1);
+        }
+        else{
+            return collisionpt(headB, headA, n1-n2);
+        } */
+
+
+
+        // OPTIMAL APPROACH
+        if(headA==NULL || headB == NULL){
+            return NULL;
+        }
+
+        ListNode*t1 = headA, *t2= headB;
+
+        while(t1!=t2){
+
+            t1 = t1->next;
+            t2 = t2->next;
+
+            if(t1==t2){
+                return t1;  //collision pt
+            }
+
+            if(t1==NULL){
+                t1 = headB;
+            }
+            if(t2 == NULL){
+                t2 = headA;
+            }
+        }
+        return t1;
+    }
